6dfb5dd68e1f516d797deb657d37ef64991a2150
Zac Buckelew <buckelewzachary@gmail.com>
2026-06-21 15:48:10 -0500

    run339.bat: stderr+stdout capture

--- run339.bat
+++ run339.bat
@@ -1,3 +1,6 @@
 @echo off
 SET SEED_PORT=2225
-C:\share\seed\seed_next_v339.exe bc3942bc > C:\share\seed\v339out.txt 2>&1
+SET VAULT_PASSPHRASE=bc3942bc
+echo Starting v339 on port 2225 > C:\share\seed\v339err.txt
+C:\share\seed\seed_next_v339.exe bc3942bc >> C:\share\seed\v339err.txt 2>&1
+echo Exit code: %ERRORLEVEL% >> C:\share\seed\v339err.txt
