3b79559d0caaf23035ff6d1f7ab676a3bbbf3b2d
Zac Buckelew <buckelewzachary@gmail.com>
2026-07-11 19:04:55 -0500

    stage: Gov_StopwatchTicks.cs

--- bridge/Gov_StopwatchTicks.cs
+++ bridge/Gov_StopwatchTicks.cs
@@ -1,20 +1,13 @@
 using System.Diagnostics;
-
 namespace Genesis.Governance;
-
 public static class StopwatchTicks
 {
     public static long ToNanoseconds(long ticks)
     {
-        if (ticks < 0)
-            throw new ArgumentOutOfRangeException(nameof(ticks));
-
-        // Quotient/remainder form avoids overflowing ticks * 1_000_000_000.
-        var wholeSeconds = ticks / Stopwatch.Frequency;
+        if (ticks < 0) throw new ArgumentOutOfRangeException(nameof(ticks));
+        var seconds = ticks / Stopwatch.Frequency;
         var remainder = ticks % Stopwatch.Frequency;
-
-        return checked(
-            wholeSeconds * 1_000_000_000L +
+        return checked(seconds * 1_000_000_000L +
             (long)((decimal)remainder * 1_000_000_000m / Stopwatch.Frequency));
     }
 }
